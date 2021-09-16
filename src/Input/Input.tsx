01f79b47a094ad6edca80845c18f401b923731bb
leejss <goldemshine+3@gmail.com>
2021-09-17 01:08:57 +0900

    first commit

---
+++ src/Input/Input.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Input.css";
+
+const Input = () => {
+  return <input className="input" />;
+};
+
+export default Input;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Button } from "./Button";
+export { Input } from "./Input";
+import "./Button/Button.css";
+import "./Input/Input.css";
